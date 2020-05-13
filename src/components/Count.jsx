63c917089deed00359fef40965ec6fbd75b62b8e
Sean Hellum <seanhellum45@gmail.com>
2020-05-13 19:10:09 +0000

    Mess around w/ components

---
+++ src/components/Count.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+var x = 0;
+var theColor = 'black';
+
+function updateColor(color) {
+    if (color === "black") {
+        return "blue"
+    } else if (color === "blue") {
+        return "red"
+    } else if (color === "red") {
+        return "green"
+    } else {
+        return "black"
+    }
+}
+
+export default function Count() {
+  return (
+    <div>
+      <h1
+        style={{
+          color: theColor,
+          paddingRight: "10%"
+        }}
+      >
+        Counter: {x}
+      </h1>
+      <button
+        onClick={() => {
+          x += 1;
+          ReactDOM.render(<App />, document.querySelector("#root"));
+        }}
+      >
+        + 1
+      </button>
+      <button
+        onClick={() => {
+          x -= 1;
+          ReactDOM.render(<App />, document.querySelector("#root"));
+        }}
+      >
+        - 1
+      </button>
+      <button
+        onClick={() => {
+          x = 0;
+          ReactDOM.render(<App />, document.querySelector("#root"));
+        }}
+      >
+        Reset
+      </button>
+      <button
+        onClick={() => {
+          theColor = updateColor(theColor);
+          ReactDOM.render(<App />, document.querySelector("#root"));
+        }}
+      >
+        Change Color
+      </button>
+    </div>
+  );
+}
